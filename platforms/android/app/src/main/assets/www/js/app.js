b2b927c586e28927faac461c095eee1c2b014aa3
Tiansi LARANJEIRA <tiansi.laranjeira@g2elab.grenoble-inp.fr>
2018-04-20 14:42:59 +0200

    Module 7 Setting up a single-page Application

--- platforms/android/app/src/main/assets/www/js/app.js
+++ platforms/android/app/src/main/assets/www/js/app.js
@@ -4,28 +4,29 @@
     /* ---------------------------------- Local Variables ---------------------------------- */
     var service = new EmployeeService();
     service.initialize().done(function () {
-        console.log("Service initialized");
+        renderHomeView();
+        console.log("Service initialized by renderHomeView");
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
-    $('.search-key').on('keyup', findByName);
-    $('.help-btn').on('click', function() {
-        alert("Employee Directory v3.4");
-    });
+    // $('.search-key').on('keyup', findByName);
+    // $('.help-btn').on('click', function() {
+    //     alert("Employee Directory v3.4");
+    // });
 
-    document.addEventListener('deviceready', function () {
-        FastClick.attach(document.body);
-        if (navigator.notification) { // Override default HTML alert with native dialog
-            window.alert = function (message) {
-                navigator.notification.alert(
-                    message, //message
-                    null, // callback
-                    "Workshop", // title
-                    'OK' // buttonName
-                );
-            };
-        }
-    }, false);
+    // document.addEventListener('deviceready', function () {
+    //     FastClick.attach(document.body);
+    //     if (navigator.notification) { // Override default HTML alert with native dialog
+    //         window.alert = function (message) {
+    //             navigator.notification.alert(
+    //                 message, //message
+    //                 null, // callback
+    //                 "Workshop", // title
+    //                 'OK' // buttonName
+    //             );
+    //         };
+    //     }
+    // }, false);
 
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
@@ -39,5 +40,16 @@
             }
         });
     }
+
+    /*  ---------------------------------- Home View markup programmatical generation ---------------------------------- */
+    function renderHomeView() {
+        var html =
+            "<h1>Répertoire</h1>" +
+            "<input class='search-key' type='search' placeholder='Enter name' />" +
+            "<ul class='employee-list'></ul>";
+        $('body').html(html);
+        $('.search-key').on('keyup', findByName);
+    }
+
 
 }());
