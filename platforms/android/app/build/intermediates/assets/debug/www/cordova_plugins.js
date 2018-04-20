d5b5f038142282720e31985fbe2de285c493ee99
Tiansi LARANJEIRA <tiansi.laranjeira@g2elab.grenoble-inp.fr>
2018-04-20 14:15:21 +0200

    init git

---
+++ platforms/android/app/build/intermediates/assets/debug/www/cordova_plugins.js
@@ -0,0 +1,37 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
+  },
+  {
+    "id": "cordova-plugin-dialogs.notification",
+    "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+    "pluginId": "cordova-plugin-dialogs",
+    "merges": [
+      "navigator.notification"
+    ]
+  },
+  {
+    "id": "cordova-plugin-dialogs.notification_android",
+    "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+    "pluginId": "cordova-plugin-dialogs",
+    "merges": [
+      "navigator.notification"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-device": "2.0.2",
+  "cordova-plugin-console": "1.1.0",
+  "cordova-plugin-dialogs": "2.0.1"
+};
+// BOTTOM OF METADATA
+});
