d2b0409bdcca6016568f609420cd45612a88bd99
Tiansi LARANJEIRA <tiansi.laranjeira@g2elab.grenoble-inp.fr>
2018-04-23 18:37:10 +0200

    9 Creating View Classes

--- platforms/android/app/build/intermediates/assets/debug/www/js/app.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/app.js
@@ -1,6 +1,10 @@
 // We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
 (function () {
 
+    /* --------------------------------- Template Variables -------------------------------- */
+    var homeTpl = Handlebars.compile($("#home-tpl").html());
+    var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
+
     /* ---------------------------------- Local Variables ---------------------------------- */
     var service = new EmployeeService();
     service.initialize().done(function () {
@@ -8,46 +12,16 @@
         console.log("Service initialized by renderHomeView");
     });
 
-    /* --------------------------------- Event Registration -------------------------------- */
-    // $('.search-key').on('keyup', findByName);
-    // $('.help-btn').on('click', function() {
-    //     alert("Employee Directory v3.4");
-    // });
-
-    // document.addEventListener('deviceready', function () {
-    //     FastClick.attach(document.body);
-    //     if (navigator.notification) { // Override default HTML alert with native dialog
-    //         window.alert = function (message) {
-    //             navigator.notification.alert(
-    //                 message, //message
-    //                 null, // callback
-    //                 "Workshop", // title
-    //                 'OK' // buttonName
-    //             );
-    //         };
-    //     }
-    // }, false);
-
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
         service.findByName($('.search-key').val()).done(function (employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i = 0; i < l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.content').html(employeeListTpl(employees));
         });
     }
 
     /*  ---------------------------------- Home View markup programmatical generation ---------------------------------- */
     function renderHomeView() {
-        var html =
-            "<h1>Répertoire</h1>" +
-            "<input class='search-key' type='search' placeholder='Enter name' />" +
-            "<ul class='employee-list'></ul>";
-        $('body').html(html);
+        $('body').html(homeTpl());
         $('.search-key').on('keyup', findByName);
     }
 
--- www/js/app.js
+++ www/js/app.js
@@ -2,28 +2,11 @@
 (function () {
 
     /* --------------------------------- Template Variables -------------------------------- */
-    var homeTpl = Handlebars.compile($("#home-tpl").html());
-    var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
-
+    HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
+    EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
     /* ---------------------------------- Local Variables ---------------------------------- */
     var service = new EmployeeService();
     service.initialize().done(function () {
-        renderHomeView();
-        console.log("Service initialized by renderHomeView");
+        $('body').html(new HomeView(service).render().$el);
     });
-
-    /* ---------------------------------- Local Functions ---------------------------------- */
-    function findByName() {
-        service.findByName($('.search-key').val()).done(function (employees) {
-            $('.content').html(employeeListTpl(employees));
-        });
-    }
-
-    /*  ---------------------------------- Home View markup programmatical generation ---------------------------------- */
-    function renderHomeView() {
-        $('body').html(homeTpl());
-        $('.search-key').on('keyup', findByName);
-    }
-
-
 }());
